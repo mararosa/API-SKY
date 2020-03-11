53c53c9c829664504c625342621c73990640fcd2
Mara Rosa <edimara.rosa@hotmail.com>
2020-03-11 11:27:31 -0300

    add dependencias require

---
+++ src/app.js
@@ -0,0 +1,8 @@
+const express = require('express') 
+const app = express()
+const cors = require('cors')
+const bodyParser = require('body-parser')
+// const dataBase = require("./models/dataBase")
+// dataBase.connect()
+
+module.exports = app
