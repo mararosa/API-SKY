90734ae02c8c1d7fa98e5ecfad1209395d5f61cb
Mara Rosa <edimara.rosa@hotmail.com>
2020-03-11 13:10:58 -0300

    add user schema datas

--- src/models/UsersSchema.js
+++ src/models/UsersSchema.js
@@ -4,14 +4,17 @@ const Schema = mongoose.Schema;
 const UsersSchema = new Schema({
     _id: { type: mongoose.Schema.Types.ObjectId, auto: true, required: true },
     nome: { type: String, required: true },
-    email: {type: String, required: true},
-    senha: { type: String, required: true },
+    email: {type: String, required: true, unique: true},
+    senha: { type: String, required: true, unique: true },
     telefone: [
         {
             numero: {type: Number, required: true },
             ddd: {type: Number, required: true}
         }
-    ]
+    ],
+    dataCadastro: { type: Date, default: Date.now},
+    dataAtualizacao: { type: Date},
+    dataLogin: { type: Date},
   })
   
   const usersModel = mongoose.model('users', UsersSchema);
