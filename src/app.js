93e2ed050c40ac96f1094c680647c7c9e8a1446c
Mara Rosa <edimara.rosa@hotmail.com>
2020-03-11 12:00:18 -0300

    add index.js

--- src/app.js
+++ src/app.js
@@ -4,5 +4,14 @@ const cors = require('cors')
 const bodyParser = require('body-parser')
 // const dataBase = require("./models/dataBase")
 // dataBase.connect()
+// const users = require('./routes/users')
+
+/////rota index
+const index = require("./routes/index")
+
+app.use(cors())
+app.use(bodyParser.json())
+app.use("/", index)
+// app.use('/users', users)
 
 module.exports = app
