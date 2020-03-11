f9b720b2d312b3b80d44a5aa48befdf3b9eed548
Mara Rosa <edimara.rosa@hotmail.com>
2020-03-11 12:36:33 -0300

    add model database - conexao com o banco

--- src/app.js
+++ src/app.js
@@ -2,8 +2,8 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 const bodyParser = require('body-parser')
-// const dataBase = require("./models/dataBase")
-// dataBase.connect()
+const dataBase = require("./models/dataBase")
+dataBase.connect()
 // const users = require('./routes/users')
 
 /////rota index
---
+++ src/models/dataBase.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose")
+const DB_URL = "mongodb://localhost:27017/ApiSky"
+
+// const DB_URL = process.env.MONGODB_URI
+
+// process.env.PORT
+
+const connect = () => {
+    mongoose.connect(DB_URL, {
+        useUnifiedTopology: true,
+        useCreateIndex: true,
+        useNewUrlParser: true
+    })
+    const connection = mongoose.connection
+    connection.on('error', () => console.error('Erro ao conectar'))
+
+    connection.once('open', () => console.log('Conectado no MongoDB'))
+}
+
+module.exports = { connect }
