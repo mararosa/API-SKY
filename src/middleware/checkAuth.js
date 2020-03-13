a8e5f98abb44a08e3f298af8156f12bebdb42def
Mara Rosa <edimara.rosa@hotmail.com>
2020-03-12 21:16:31 -0300

    add autrizacao nas rotas

--- src/middleware/checkAuth.js
+++ src/middleware/checkAuth.js
@@ -2,26 +2,14 @@ const jwt = require('jsonwebtoken');
 const SECRET = "umEHhDyErggnyUWMSQL1SsBgVc309uELyJVr1Wyrnc0oztFHuaVfu3GqNYyo9J8";
 
 module.exports = (request, response, next) => {
-    const authHeader = request.get('authorization')
-    let autenticado = false
-    if (!authHeader) {
-        return response.status(401).json({
-            message: 'Você precisa fazer login'
-        });
-    }
-    const token = authHeader.split(' ')[1]
-    jwt.verify(token, SECRET, (error, decoded) => {
-        if (error) {
-            autenticado = false
-        } else {
-            autenticado = true
-        }
-    }
-    )
-    if (!autenticado) {
-        return response.status(403).json({
-            message: 'Não autorizado'
-        });
-    }
-    next()
-};
+try {
+    const token = request.headers.authorization.split(" ")[1];
+    const decoded = jwt.verify(token, SECRET);
+    request.userData = decoded;
+    next();
+} catch (error) {
+    return response.status(401).json({
+        message: 'Não Autorizado'
+    });
+}
+}
