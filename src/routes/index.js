b2bb82ee53a784f601e9d3cb4ca8195620853ada
Mara Rosa <edimara.rosa@hotmail.com>
2020-03-13 16:45:12 -0300

    add author

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,7 +4,8 @@ const router = express.Router();
 
 router.get("/", function(req, res) {
   res.status(200).send({
-    title: "Hello Dev",
+    title: " Hello Dev :) ",
+    author: "Mara Rosa",
     version: "0.0.1"
   });
 });
