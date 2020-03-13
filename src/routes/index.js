d241a8be51b45d58360756afa65ed39aa5e1fdb7
Mara Rosa <edimara.rosa@hotmail.com>
2020-03-13 16:41:26 -0300

    eslint

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,11 @@
-const express = require("express")
-const router = express.Router()
+const express = require("express");
+const router = express.Router();
 
 router.get("/", function(req, res) {
   res.status(200).send({
     title: "Hello Dev",
     version: "0.0.1"
-  })
-})
+  });
+});
 
-module.exports = router
+module.exports = router;
